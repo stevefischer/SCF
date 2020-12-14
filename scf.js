630fbc9bd3b8403688743b4747771e8cc999e5e4
stevefischer <stevefischer@student.olympic.edu>
2020-12-13 21:29:24 -0800

    Multiple Changes

---
+++ scf.js
@@ -0,0 +1,392 @@
+/*
+    Name: Steve's Country Favorites
+    Author: Steve Fischer
+    Date: 12/13/2020
+    Other Files: about.htm, artists.htm, index.htm, songs.htm, styles.css
+*/
+
+
+/** Artist Page **/
+
+    /*** Darius Script ***/
+    $(document).ready(function(){
+    $("#one").click(function(){
+    $("#darius").slideDown(1500);
+    $("#dariusText").fadeIn(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+    
+    /*** Brad Script ***/
+    $(document).ready(function(){
+    $("#two").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideDown(1500);
+    $("#bradText").fadeIn(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+
+    /*** Blaine Script ***/
+    $(document).ready(function(){
+    $("#three").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideDown(1500);
+    $("#blaineText").fadeIn(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+   
+    /*** Ashley Script ***/
+    $(document).ready(function(){
+    $("#four").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeIn(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+   
+    /*** Kelsea Script ***/
+    $(document).ready(function(){
+    $("#five").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideDown(1500);
+    $("#kelseaText").fadeIn(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+   
+       /*** Carly Script ***/
+    $(document).ready(function(){
+    $("#six").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideDown(1500);
+    $("#carlyText").fadeIn(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });  
+
+       /*** M & T Script ***/
+    $(document).ready(function(){
+    $("#seven").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideDown(1500);
+    $("#tae").slideDown(1500);
+    $("#mandtText").fadeIn(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+
+    /** Chris Script ***/
+    $(document).ready(function(){
+    $("#eight").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideDown(1500);
+    $("#chrisText").fadeIn(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+
+       /*** MG Script ***/
+    $(document).ready(function(){
+    $("#nine").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeIn(1500);
+    $("#sugar").slideUp(1500);
+    $("#sugarText").fadeOut(1500);
+    });
+    });
+
+       /*** Sugarland Script ***/
+    $(document).ready(function(){
+    $("#ten").click(function(){
+    $("#darius").slideUp(1500);
+    $("#dariusText").fadeOut(1500);
+    $("#brad").slideUp(1500);
+    $("#bradText").fadeOut(1500);
+    $("#blaine").slideUp(1500);
+    $("#blaineText").fadeOut(1500);
+    $("#ashleyText").fadeOut(1500); 
+    $("#kelsea").slideUp(1500);
+    $("#kelseaText").fadeOut(1500); 
+    $("#carly").slideUp(1500);
+    $("#carlyText").fadeOut(1500);
+    $("#maddie").slideUp(1500);
+    $("#tae").slideUp(1500);
+    $("#mandtText").fadeOut(1500);
+    $("#chris").slideUp(1500);
+    $("#chrisText").fadeOut(1500);
+    $("#montText").fadeOut(1500);
+    $("#sugar").slideDown(1500);
+    $("#sugarText").fadeIn(1500);
+    });
+    });
+
+/** Songs Page **/
+
+/* Wagon Wheel*/
+ $(document).ready(function(){
+ $("#one").click(function(){
+ $("#wagon").slideDown();
+ $("#happy").slideUp();
+ $("#great").slideUp();
+ $("#feet").slideUp();
+ $("#limit").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+   
+/* Happy Now */
+ $(document).ready(function(){
+ $("#two").click(function(){
+ $("#happy").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#great").slideUp();
+ $("#feet").slideUp();
+ $("#limit").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+ 
+/* Great Day */
+$(document).ready(function(){
+ $("#three").click(function(){
+ $("#great").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#feet").slideUp();
+ $("#limit").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+    
+/* Six Feet */
+$(document).ready(function(){
+ $("#four").click(function(){
+ $("#feet").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#great").slideUp();
+ $("#limit").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+
+/* Limitless */
+$(document).ready(function(){
+ $("#five").click(function(){
+ $("#limit").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#feet").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+  
+/* Check y/n */
+$(document).ready(function(){
+ $("#six").click(function(){
+ $("#check").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#feet").slideUp();
+ $("#limit").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+ 
+/* If Merle */
+$(document).ready(function(){
+ $("#seven").click(function(){
+ $("#merle").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#feet").slideUp();
+ $("#check").slideUp();
+ $("#limit").slideUp();
+ $("#storm").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+    
+/* Storm Blows */
+$(document).ready(function(){
+ $("#eight").click(function(){
+ $("#storm").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#feet").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#limit").slideUp();
+ $("#proud").slideUp();
+ });
+ });
+
+/* Be Proud*/
+                
+$(document).ready(function(){       
+ $("#nine").click(function(){
+ $("#proud").slideDown("slow");
+ $("#wagon").slideUp();
+ $("#happy").slideUp();
+ $("#feet").slideUp();
+ $("#check").slideUp();
+ $("#merle").slideUp();
+ $("#storm").slideUp();
+ $("#limit").slideUp();
+ });
+ });
